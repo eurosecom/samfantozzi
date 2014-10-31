8becb66ee25514c3277f41fc19c6b3a50aef8d2b
eurosecom <eurosecom1@gmail.com>
2014-10-31 12:32:36 +0100

    Distributed cashInvoices - pager of cashReceiptes
    Modify screen of cashReceiptes

--- src/com/eusecom/samfantozzi/ImageAdapterFromRes.java
+++ src/com/eusecom/samfantozzi/ImageAdapterFromRes.java
@@ -14,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -29,14 +30,19 @@ public class ImageAdapterFromRes extends PagerAdapter {
         ImageView imageView;
         TextView textreklama1;
         Button btnZoznam;
+        Button btnUprav;
         public Activity activity;
  
-        private int[] GalImages = new int[] { 
+
+		@SuppressWarnings("unused")
+		private int[] GalImages = new int[] { 
                 //Images from resource folder.
             R.drawable.one,  
             R.drawable.two,
             R.drawable.three
         };
+        
+        //int icount =textArray.size();
  
         ImageAdapterFromRes(Context context, ArrayList<String> textArray, ArrayList<String> urlArray, ArrayList<Bitmap> bitmapArray, OnPagerItemSelected listener){
             this.context=context;
@@ -49,7 +55,8 @@ public class ImageAdapterFromRes extends PagerAdapter {
  
         @Override
         public int getCount() {
-            return GalImages.length;
+            //return GalImages.length;
+            return textArray.size();
         }
  
         public Object instantiateItem(ViewGroup collection, int position) {
@@ -61,10 +68,37 @@ public class ImageAdapterFromRes extends PagerAdapter {
         	 View view = inflater.inflate(R.layout.reklamator_new, null);
         	 
         	 imageView = (ImageView) view.findViewById(R.id.imgreklama1);
-        	 imageView.setImageBitmap(bitmapArray.get(position));
+        	 imageView.setImageBitmap(bitmapArray.get(1));
         	 
-        	 textreklama1 = (TextView) view.findViewById(R.id.textreklama1);
-        	 textreklama1.setText(textArray.get(position).toString());
+        	 //String datatxt = i2 + " ;" + uce + " ;" + pozx + " ;" + dokladx + " ;" + dat + " ;" + ico + " ;" + fak + " ;" + kto
+             //+ " ;" + txp + " ;" + zk0 + " ;" + zk1 + " ;" + zk2 + " ;" + dn1 + " ;" + dn2 + " ;" + poh + " ;" + clk + " \n";
+
+        	 	String indexx = textArray.get(position).toString();
+        	 	String delims2 = "[;]+";
+         		String[] riadokxxx = indexx.split(delims2);
+         		String ucex =  riadokxxx[1].trim();
+         		String pohx =  riadokxxx[2].trim();
+         		String dokx =  riadokxxx[3].trim();
+         		String datx =  riadokxxx[4].trim();
+         		
+         		textreklama1 = (TextView) view.findViewById(R.id.textreklama1);         		
+         		if( pohx.equals("1")) {
+         		textreklama1.setText(((Activity) context).getResources().getString(R.string.popisprijmovy));
+         		}else{
+         			textreklama1.setText(((Activity) context).getResources().getString(R.string.popisvydavkovy));	
+         		}
+         		
+         		EditText inputUce = (EditText) view.findViewById(R.id.inputUce);
+         		inputUce.setText(ucex);
+         		
+         		EditText inputDat = (EditText) view.findViewById(R.id.inputDat);
+         		inputDat.setText(datx);
+         		
+         		EditText inputDok = (EditText) view.findViewById(R.id.inputDok);
+         		inputDok.setText(dokx);
+         		
+
+
         	 
         	 btnZoznam = (Button) view.findViewById(R.id.btnZoznam);
         	 btnZoznam.setOnClickListener(new View.OnClickListener() {
@@ -77,6 +111,29 @@ public class ImageAdapterFromRes extends PagerAdapter {
                      extras.putString("cat", "1");
                      extras.putString("dcex", "0");
                      extras.putString("pagex", "1");
+                     i.putExtras(extras);
+                     context.startActivity(i);
+                     //it is running ok but better is implement interface
+                     //((Activity) context).finish();
+                     mListener.pagerItemSelected();
+   
+                 }
+                 
+                 
+             });
+        	 
+        	 btnUprav = (Button) view.findViewById(R.id.btnUprav);
+        	 btnUprav.setOnClickListener(new View.OnClickListener() {
+  
+                 @Override
+                 public void onClick(View v) {
+
+                	 Intent i = new Intent(v.getContext(), NewPoklZahActivitySD.class);
+                     Bundle extras = new Bundle();
+                     extras.putString("pozx", "1");
+                     extras.putString("fakx", "10003");
+                     extras.putString("newx", "0");
+                     extras.putString("cat", "1");
                      i.putExtras(extras);
                      context.startActivity(i);
                      //it is running ok but better is implement interface
