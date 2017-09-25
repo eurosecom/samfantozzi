461494ebdcce72d4461968ee507e8e0a90145d83
eurosecom <eurosecom1@gmail.com>
2017-09-25 15:33:06 +0200

    Samfantozzi Main Activity
    #1
    - MainActivity.kt and SamfantoyyiApp.java

---
+++ app/src/main/java/com/eusecom/samfantozzi/SamfantozziApp.java
@@ -0,0 +1,17 @@
+package com.eusecom.samfantozzi;
+
+import android.app.Application;
+
+
+public class SamfantozziApp extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+
+
+}
