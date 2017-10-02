862616e24b757bde5206bdc5dbaebf084a0ad580
eurosecom <eurosecom1@gmail.com>
2017-10-02 21:58:16 +0200

    SamfantozziApp override Application
    #2
    - multidex support in AndroidManifest.xml SamFantoyyiApp.java instead of
    MultiDexApplication

--- app/src/main/java/com/eusecom/samfantozzi/SamfantozziApp.java
+++ app/src/main/java/com/eusecom/samfantozzi/SamfantozziApp.java
@@ -1,28 +1,32 @@
 package com.eusecom.samfantozzi;
 
 import android.app.Activity;
-import android.app.Application;
 import android.support.annotation.NonNull;
-
+import android.support.multidex.MultiDexApplication;
 import com.eusecom.samfantozzi.dagger.components.DaggerDemoComponent;
 import com.eusecom.samfantozzi.dagger.components.DemoComponent;
 import com.eusecom.samfantozzi.dagger.modules.ApplicationModule;
 import com.eusecom.samfantozzi.di.DaggerAppComponent;
-import com.google.firebase.FirebaseApp;
+import com.eusecom.samfantozzi.mvvmschedulers.ISchedulerProvider;
+import com.eusecom.samfantozzi.mvvmschedulers.SchedulerProvider;
+import com.eusecom.samfantozzi.rxbus.RxBus;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.leakcanary.LeakCanary;
-
 import javax.inject.Inject;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasActivityInjector;
 
 
-public class SamfantozziApp extends Application implements HasActivityInjector {
+public class SamfantozziApp extends MultiDexApplication implements HasActivityInjector {
 
     @Inject
     DispatchingAndroidInjector<Activity> activityDispatchingAndroidInjector;
 
+    public RxBus _rxBus;
+
+    @NonNull
+    private DatabaseReference mDatabaseReference;
 
     @Override
     public void onCreate() {
@@ -34,6 +38,8 @@ public class SamfantozziApp extends Application implements HasActivityInjector {
             return;
         }
         LeakCanary.install(this);
+
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
         DaggerAppComponent
                 .builder()
@@ -48,6 +54,26 @@ public class SamfantozziApp extends Application implements HasActivityInjector {
         return activityDispatchingAndroidInjector;
     }
 
+
+
+
+    @NonNull
+    public ISchedulerProvider getSchedulerProvider() {
+        return SchedulerProvider.getInstance();
+    }
+
+    @NonNull
+    public DatabaseReference getDatabaseFirebaseReference() {
+        return mDatabaseReference;
+    }
+
+
+    public RxBus getRxBusSingleton() {
+        if (_rxBus == null) {
+            _rxBus = new RxBus();
+        }
+        return _rxBus;
+    }
 
     private final DemoComponent dgaeacomponent = createDgAeaComponent();
 
