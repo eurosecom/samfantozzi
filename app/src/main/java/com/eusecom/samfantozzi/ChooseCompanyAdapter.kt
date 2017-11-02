f55689adc3732d41ab25994a36ac61cd960ae57e
eurosecom <eurosecom1@gmail.com>
2017-11-02 11:09:34 +0100

    Choose company activity
    #7
    - change color by clicking items in recyclerview

--- app/src/main/java/com/eusecom/samfantozzi/ChooseCompanyAdapter.kt
+++ app/src/main/java/com/eusecom/samfantozzi/ChooseCompanyAdapter.kt
@@ -4,12 +4,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.item_android_version.view.*
+import kotlinx.android.synthetic.main.item_choosecompany.view.*
 
 class ChooseCompanyAdapter(var myAndroidOSList: List<CompanyKt>, val listener: (CompanyKt) -> Unit) : RecyclerView.Adapter<ChooseCompanyAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChooseCompanyAdapter.ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_android_version, parent, false)
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_choosecompany, parent, false)
         return ViewHolder(v)
     }
 
