b91a447b59acd85de5d9484798f34691e3692d0e
eurosecom <eurosecom1@gmail.com>
2017-11-09 11:20:29 +0100

    CashListActivity.java cash documents list
    #4
    - implement rxBus to CashListKtFragment
    - implement call MVVM mSubscription?.add(mViewModel.myAbsencesFromServer

--- app/src/main/java/com/eusecom/samfantozzi/CashListKtFragment.kt
+++ app/src/main/java/com/eusecom/samfantozzi/CashListKtFragment.kt
@@ -5,13 +5,23 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.*
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
+import com.eusecom.samfantozzi.models.Attendance
 import com.eusecom.samfantozzi.rxbus.RxBus
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import org.jetbrains.anko.support.v4.toast
+import rx.Observable
+import rx.functions.Action1
+import rx.schedulers.Schedulers
+import rx.subscriptions.CompositeSubscription
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 /**
@@ -29,6 +39,9 @@ class CashListKtFragment : Fragment() {
     protected var mSearchButton: Button? = null
     private var mProgressBar: ProgressBar? = null
 
+    private var mSubscription: CompositeSubscription? = null
+    private var _disposables = CompositeDisposable()
+
     @Inject
     lateinit var mSharedPreferences: SharedPreferences
 
@@ -38,6 +51,40 @@ class CashListKtFragment : Fragment() {
     @Inject
     lateinit var  _rxBus: RxBus
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        (activity.application as SamfantozziApp).dgaeacomponent().inject(this)
+
+        val tapEventEmitter = _rxBus.asFlowable().publish()
+
+        _disposables
+                .add(tapEventEmitter.subscribe { event ->
+                    if (event is DgAeaListFragment.ClickFobEvent) {
+                        Log.d("CashListKtActivity  ", " fobClick ")
+
+                    }
+                    if (event is Attendance) {
+
+                        val usnamex = event.getUsname()
+
+                        Log.d("CashListKtFragment ", usnamex)
+                        //String serverx = "DgAeaListFragment " + usnamex;
+                        //Toast.makeText(getActivity(), serverx, Toast.LENGTH_SHORT).show();
+
+
+                    }
+
+                })
+
+        _disposables
+                .add(tapEventEmitter.publish { stream -> stream.buffer(stream.debounce(1, TimeUnit.SECONDS)) }
+                        .observeOn(AndroidSchedulers.mainThread()).subscribe { taps ->
+                    ///_showTapCount(taps.size()); OK
+                })
+
+        _disposables.add(tapEventEmitter.connect())
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -58,7 +105,7 @@ class CashListKtFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        (activity.application as SamfantozziApp).dgaeacomponent().inject(this)
+        //(activity.application as SamfantozziApp).dgaeacomponent().inject(this)
 
         val umex = mSharedPreferences.getString("ume", "")
         toast(" umex " + umex)
@@ -70,8 +117,51 @@ class CashListKtFragment : Fragment() {
         mRecycler?.setLayoutManager(mManager)
         mRecycler?.setAdapter(mAdapter)
 
-
     }//end of onActivityCreated
+
+    override fun onResume() {
+        super.onResume()
+        bind()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unBind()
+    }
+
+    private fun bind() {
+        mSubscription = CompositeSubscription()
+
+        mSubscription?.add(mViewModel.myAbsencesFromServer
+                .subscribeOn(Schedulers.computation())
+                .observeOn(rx.android.schedulers.AndroidSchedulers.mainThread())
+                .doOnError { throwable -> Log.e("CashListKtFragment", "Error Throwable " + throwable.message) }
+                .onErrorResumeNext({ throwable -> Observable.empty() })
+                .subscribe({ it -> setAbsences(it) }))
+
+    }
+
+    private fun unBind() {
+        mSubscription?.unsubscribe()
+        mSubscription?.clear()
+
+    }
+
+    private fun setAbsences(attendances: List<Attendance>) {
+        mAdapter?.setAbsserver(attendances)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        //_disposables.dispose()
+        //if (mDisposable != null) {
+        //    mDisposable.dispose()
+        //}
+        mAdapter = AbsServerAsAdapter(_rxBus)
+        mSubscription?.unsubscribe()
+        mSubscription?.clear()
+
+    }
 
 
 }
