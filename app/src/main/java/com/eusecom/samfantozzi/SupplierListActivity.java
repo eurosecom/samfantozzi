3a82ca359e0c4bb0c6fe103f035cc5be103bfcb2
eurosecom <eurosecom1@gmail.com>
2017-11-21 11:30:37 +0100

    SupplierListActivity
    #8
    - searchview in appbar activity

--- app/src/main/java/com/eusecom/samfantozzi/SupplierListActivity.java
+++ app/src/main/java/com/eusecom/samfantozzi/SupplierListActivity.java
@@ -16,14 +16,22 @@
 
 package com.eusecom.samfantozzi;
 
+import android.app.SearchManager;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
 import javax.inject.Inject;
 
 
@@ -132,6 +140,48 @@ public class  SupplierListActivity extends BaseListActivity {
 
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Retrieve the SearchView and plug it into SearchManager
+        getMenuInflater().inflate(R.menu.menu_listdoc, menu);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // use this method when query submitted
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // use this method for auto complete search process
+                Toast.makeText(SupplierListActivity.this, newText, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+
+            Intent is = new Intent(getApplicationContext(), SettingsActivity.class);
+            startActivity(is);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 
 }
--- app/src/main/java/com/eusecom/samfantozzi/SupplierListFragment.java
+++ app/src/main/java/com/eusecom/samfantozzi/SupplierListFragment.java
@@ -1,6 +1,7 @@
 package com.eusecom.samfantozzi;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -12,11 +13,15 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
 import com.eusecom.samfantozzi.rxbus.RxBus;
 import java.util.Collections;
 import java.util.List;
@@ -37,6 +42,7 @@ import rx.schedulers.Schedulers;
 import rx.subscriptions.CompositeSubscription;
 
 import static android.content.ContentValues.TAG;
+import static java.lang.System.out;
 import static rx.Observable.empty;
 
 
@@ -377,5 +383,6 @@ public class SupplierListFragment extends Fragment {
         builder.show();
 
     }
+
 
 }
